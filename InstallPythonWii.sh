43762446fbfbec2d573f136f8fe9abb5e1a7230e
Matthew Sage <mattsage@users.noreply.github.com>
2017-05-25 20:26:58 +0100

    Create InstallPythonWii.sh

---
+++ InstallPythonWii.sh
@@ -0,0 +1,6 @@
+#Type hcitool scan and then press the '1' and '2' buttons on your Wiimote at the same time. The blue LEDs should flash on the Wiimote and you should see something like this on screen:
+#Scanning ... 00:1E:02:8A:CD:A1 Nintendo RVL-CNT-01
+
+sudo apt-get install python-cwiid
+
+#Example Wii Code here: https://github.com/raspberrypilearning/robo-butler/blob/master/code/wii_remote_1.py
